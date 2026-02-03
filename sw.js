6d6715cf0063ec08cb6bbf676483c1c593197bac
牛小牛 <2691778746@qq.com>
2026-02-03 09:54:03 +0800

    Site updated: 2026-02-03 09:54:03

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1770051736267";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1770083591193";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
